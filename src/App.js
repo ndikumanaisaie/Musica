f0df35760b5fb15c61222a15749a6b9849c4d5d9
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-07-07 13:25:09 +0100

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import { Box } from '@mui/material';
+
+import './App.css';
+
+const App = () => (
+  <div>Hallo React</div>
+);
+
+export default App;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'd7ec23a06fmsh3e7bf70a809778ap18b7f3jsna5e55b18f5e3',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
